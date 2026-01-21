4f1d0689d4d7f2aba4351363b9144db45b624da3
Darandin <darandin@gmail.com>
2026-01-21 16:53:10 -0500

    Initial Commit: Adjustments to Mob Behavior, Abilities, and Difficulty
    Firesteel Golem - A juggernaut that pushes enemies around instead of getting pushed
    Magma Toad - Slower and less accurate against moving targets
    Yeti - A juggernaut that pushes enemies around instead of getting pushed
    Scorpion - Draws enemies in with its claws to finish them off with its stinger
    Scarak Seeker - Uses its agility in the air to reposition for deadly flyovers
    Scarak Louse - Small and weak with a lasting bite
    Scarak Fighter - Strikes quick from a safe distance to surprise unsuspecting prey
    Many enemies now become aggressive from further distances when attacked
    
    Knockback reduced for most weapons
    
    Gathering ores now requires the appropriate tier pickaxe
    Fists -> Stone -> Copper -> Iron -> Cobalt/Thorium -> Adamantite -> Mithril -> Onyxium

---
+++ src/main/java/com/adventuremod/plugin/Main.java
@@ -0,0 +1,16 @@
+package com.adventuremod.plugin;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+public class Main extends JavaPlugin {
+    public Main(@NonNullDecl JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup(){
+        super.setup();
+    }
+}
